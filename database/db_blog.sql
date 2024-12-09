2d300b74c816ffe22df0784fb76e4edc300b56c9
Danny Satria <danny.satriaku@gmail.com>
2024-12-10 06:05:28 +0700

    replace db

--- database/db_blog.sql
+++ database/db_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 06, 2024 at 06:06 AM
+-- Generation Time: Dec 09, 2024 at 11:00 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -29,12 +29,44 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `post` (
   `id_post` int NOT NULL,
-  `tgl_post` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `tgl_post` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `judul` varchar(255) NOT NULL,
   `artikel` text NOT NULL,
   `gambar` varchar(255) NOT NULL,
-  `id_admin` int NOT NULL
+  `penulis` varchar(100) NOT NULL,
+  `views` int UNSIGNED NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id_post`, `tgl_post`, `judul`, `artikel`, `gambar`, `penulis`, `views`) VALUES
+(15, '2024-12-09 18:32:27', 'Rehan Wangsaf Nyatakan Mengundurkan Diri Dari Dunia Perjomokan', 'jdsaodj osdjaosdjasodjas odjaos djasodjasdjasdoasdoasd asdasdasidasidhasoidhaisdhaiso haisdhaishdasdhas hasi dhasdhaisdhasidh ashdaisdhasihd iashdaishdaisdhashdaish iashdaishdasihdasihdasi hsaidhsadhasdhashd iashdaishdsaihdashdasi hasidhasdhashdash dasidhsadhasdhas dhasidhasdhasohd ashdaishdsahdasi hasdhasdhasdhasi dhas dhasdhasdhasidhasihdsa', '../assets/upload/furina ajg.png', 'Esteh', 39),
+(16, '2024-12-09 18:35:31', 'Rekomendasi 10 Makanan Khas Ngawi', 'aasguag uasdg ausdgasudgaus gdsaudgasudgasudgausdg uasgdasgdasugdausgdausdg asudgasudgasdgasu dgasudgasdgasdasyudgasydgasy dgasydgasyudgasydgays gdasyudgaysdgasdgasydgayusdg asydgasydgasydgasydgaysdg aysgdaysdgasydgasy dgasydgasdgasdygasydg asdgasdgasydg asdgasdgasydgasydgashdgasydgas dgasydg asdgasdgasydg asyudgasydashdasdg aysd asdyuasgyudasydg asydsayd gasydasyd gasydgasydasy dgsa', '../assets/upload/WhatsApp Image 2024-03-01 at 20.03.03_e1771e79.jpg', 'Somay', 27);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id_profile` int NOT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `deskripsi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `foto_profile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `kontak` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `tgl_dibuat` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id_profile`, `nama`, `role`, `deskripsi`, `foto_profile`, `kontak`, `tgl_dibuat`) VALUES
+(61, 'Zidane', 'Frontend Dev', 'halo sajsaosjaosjaosj aojs aosjaojs aojsao jsao sjao ao jaos jaos jao jsaosjaos jaos jaos jao jao jasjao sjao jsao a osa sa sa salam kenal', '6756d47955fca_arisu hand punch.jpeg', 'dfasdasdasdadasdsaddadasd', '2024-12-09 11:28:57');
 
 -- --------------------------------------------------------
 
@@ -67,6 +99,12 @@ ALTER TABLE `post`
   ADD PRIMARY KEY (`id_post`);
 
 --
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id_profile`);
+
+--
 -- Indexes for table `tb_admin`
 --
 ALTER TABLE `tb_admin`
@@ -80,7 +118,13 @@ ALTER TABLE `tb_admin`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id_post` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_post` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id_profile` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `tb_admin`
